e977f05d7f623a09f94a819242eabb2f130654ef
Zakaria-cpu301 <ramadanzaka27@gmail.com>
2025-07-07 16:05:53 +0700

    fix: finishing basic website with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'global-page': 'rgb(1, 41, 103)',
+      },
+    },
   },
   plugins: [],
 };
